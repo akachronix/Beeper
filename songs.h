2b55ef8b086f4b256a64fdc424d0c14e210c9228
Brendan Gowen <bgowengaming1@gmail.com>
2017-02-03 19:26:11 -0500

    Create songs.h

---
+++ songs.h
@@ -0,0 +1,91 @@
+#ifndef SONGS_H
+#define SONGS_H
+
+#include <windows.h>
+#include "notes.h"
+
+void Mary() {
+
+    Beep(B4, 300);
+    Beep(A4, 300);
+    Beep(G4, 300);
+    Beep(A4, 300);
+    Beep(B4, 300);
+    Beep(B4, 300);
+    Beep(B4, 300);
+    Sleep(300);
+    Beep(A4, 300);
+    Beep(A4, 300);
+    Beep(A4, 300);
+    Sleep(300);
+    Beep(B4, 300);
+    Beep(D5, 300);
+    Beep(D5, 300);
+    Sleep(300);
+    Beep(B4, 300);
+    Beep(A4, 300);
+    Beep(G4, 300);
+    Beep(A4, 300);
+    Beep(B4, 300);
+    Beep(B4, 300);
+    Beep(B4, 300);
+    Sleep(300);
+    Beep(A4, 300);
+    Beep(A4, 300);
+    Beep(B4, 300);
+    Beep(A4, 300);
+    Beep(G4, 300);
+}
+
+void Type() {
+    Beep(G5, 500);
+    Beep(G5, 500);
+    Beep(G5, 250);
+    Beep(F5, 250);
+    Beep(E5, 250);
+    Sleep(250);
+    Beep(D5, 500);
+    Beep(D5, 500);
+    Beep(D5, 250);
+    Beep(C5, 250);
+    Beep(B4, 250);
+    Sleep(250);
+    Beep(G5, 500);
+    Beep(G5, 500);
+    Beep(G5, 250);
+    Beep(F5, 250);
+    Beep(E5, 250);
+    Sleep(250);
+    Beep(D5, 500);
+    Beep(D5, 500);
+    Beep(D5, 250);
+    Beep(C5, 250);
+    Beep(B4, 250);
+    Sleep(250);
+    Beep(A4, 500);
+    Beep(A4, 500);
+    Beep(A4, 250);
+    Beep(G4, 250);
+    Beep(F4, 250);
+    Sleep(250);
+    Beep(E4, 500);
+    Beep(E4, 500);
+    Beep(E4, 250);
+    Beep(D4, 250);
+    Beep(C4, 250);
+    Sleep(250);
+    Beep(A4, 500);
+    Beep(A4, 500);
+    Beep(A4, 250);
+    Beep(G4, 250);
+    Beep(F4, 250);
+    Sleep(250);
+    Beep(E4, 500);
+    Beep(E4, 500);
+    Beep(E4, 250);
+    Beep(D4, 250);
+    Beep(C4, 250);
+    Sleep(250);
+}
+
+#endif // SONGS_H
