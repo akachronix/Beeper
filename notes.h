cbee950b29dfdc1bfc916c1ba58ccebeaeb20d1f
Brendan Gowen <bgowengaming1@gmail.com>
2017-02-03 19:26:35 -0500

    Create notes.h

---
+++ notes.h
@@ -0,0 +1,61 @@
+// Name:   notes.h
+// Author: Chronix
+// This defines musical notes
+// as constants to include in
+// Beeper.
+
+// To be used with Beeper
+
+#ifndef NOTES_H
+#define NOTES_H
+
+// First Octave
+#define C0  16.35f
+#define Db0 17.32f
+#define D0  18.35f
+#define Eb0 19.45f
+#define E0  20.60f
+#define F0  21.83f
+#define Gb0 23.12f
+#define G0  24.50f
+#define Ab0 25.96f
+#define A0  27.50f
+#define Bb0 29.14f
+#define B0  30.87f
+
+// Second Octave
+#define C1  32.70f
+#define Db1 34.65f
+#define D1  36.71f
+#define Eb1 38.89f
+#define E1  41.20f
+#define F1  43.65f
+#define Gb1 46.25f
+#define G1  49.00f
+#define Ab1 51.91f
+#define A1  55.00f
+#define Bb1 58.27f
+#define B1  61.74f
+
+// Fourth Octave
+#define C4  261.63f
+#define Db4 277.18f
+#define D4  293.66f
+#define Eb4 311.13f
+#define E4  329.63f
+#define F4  349.23f
+#define Gb4 369.99f
+#define G4  392.00f
+#define Ab4 415.30f
+#define A4  440.00f
+#define Bb4 466.16f
+#define B4  493.88f
+
+// Fifth Octave
+#define C5  523.25f
+#define D5  587.33f
+#define E5  659.25f
+#define F5  698.46f
+#define G5  783.99f
+
+#endif
