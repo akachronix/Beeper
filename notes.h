e1220828591fd91cb2a41b3ee947d78851cb0483
Brendan Gowen <bgowengaming1@gmail.com>
2017-02-05 20:13:21 -0500

    Update notes.h

--- notes.h
+++ notes.h
@@ -4,7 +4,7 @@
 // as constants to include in
 // Beeper.
 
-// To be used with Beeper
+// To be used with Beeper.
 
 #ifndef NOTES_H
 #define NOTES_H
@@ -37,7 +37,35 @@
 #define Bb1 58.27f
 #define B1  61.74f
 
+// Third Octave
+#define C2  65.41f
+#define Db2 69.30f
+#define D2  73.42f
+#define Eb2 77.78f
+#define E2  82.41f
+#define F2  87.31f
+#define Gb2 92.50f
+#define G2  98.00f
+#define Ab2 103.83f
+#define A2  110.00f
+#define Bb2 116.54f
+#define B2  123.47f
+
 // Fourth Octave
+#define C3  130.81f
+#define Db3 138.59f
+#define D3  146.83f
+#define Eb3 155.56f
+#define E3  164.81f
+#define F3  174.61f
+#define Gb3 185.00f
+#define G3  196.00f
+#define Ab3 207.65f
+#define A3  220.00f
+#define Bb3 233.08f
+#define B3  246.94f
+
+// Fifth Octave
 #define C4  261.63f
 #define Db4 277.18f
 #define D4  293.66f
@@ -51,11 +79,60 @@
 #define Bb4 466.16f
 #define B4  493.88f
 
-// Fifth Octave
+// Sixth Octave
 #define C5  523.25f
+#define Db5 554.37f
 #define D5  587.33f
+#define Eb5 622.25f
 #define E5  659.25f
 #define F5  698.46f
+#define Gb5 739.99f
 #define G5  783.99f
+#define Ab5 830.61f
+#define A5  880.00f
+#define Bb5 932.33f
+#define B5  987.77f
+
+// Seventh Octave
+#define C6  1046.50f
+#define Db6 1108.73f
+#define D6  1174.66f
+#define Eb6 1244.51f
+#define E6  1318.51f
+#define F6  1396.91f
+#define Gb6 1479.98f
+#define G6  1567.98f
+#define Ab6 1661.22f
+#define A6  1760.00f
+#define Bb6 1864.66f
+#define B6  1975.53f
+
+// Eighth Octave
+#define C7  2093.00f
+#define Db7 2217.46f
+#define D7  2349.32f
+#define Eb7 2489.02f
+#define E7  2637.02f
+#define F7  2793.83f
+#define Gb7 2959.96f
+#define G7  3135.96f
+#define Ab7 3322.44f
+#define A7  3520.00f
+#define Bb7 3729.31f
+#define B7  3951.07f
+
+// Ninth Octave
+#define C8  4186.01f
+#define Db8 4434.92f
+#define D8  4698.63f
+#define Eb8 4978.03f
+#define E8  5274.04f
+#define F8  5587.65f
+#define Gb8 5919.91f
+#define G8  6271.93f
+#define Ab8 6644.88f
+#define A8  7040.00f
+#define Bb8 7458.62f
+#define B8  7902.13f
 
 #endif
